8a83091e4fc91625d66c6d29a567fd949ec51ea0
Utkarsh <ignition3951@gmail.com>
2025-02-06 13:30:42 +0530

    IMplement basic jdbc connection

---
+++ Chapter6/src/main/java/com/utk/config/SimpleDataSourceConfig.java
@@ -0,0 +1,51 @@
+package com.utk.config;
+
+import javax.sql.DataSource;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.jdbc.datasource.SimpleDriverDataSource;
+
+import com.mysql.cj.jdbc.Driver;
+//import org.springframework;
+
+@Configuration
+@PropertySource("classpath:db/jdbc.properties")
+public class SimpleDataSourceConfig {
+
+	private static Logger logger = LoggerFactory.getLogger(SimpleDataSourceConfig.class);
+
+	@Value("${jdbc.driverClassName}")
+	private String driverClassName;
+
+	@Value("${jdbc.url}")
+	private String url;
+
+	@Value("${jdbc.username}")
+	private String username;
+
+	@Value("${jdbc.password}")
+	private String password;
+
+	@Bean
+	public DataSource dataSource() {
+		try {
+			SimpleDriverDataSource dataSource = new SimpleDriverDataSource();
+			Class<? extends Driver> driver = (Class<? extends Driver>) Class.forName(driverClassName);
+			dataSource.setDriverClass(driver);
+			dataSource.setUrl(url);
+			dataSource.setUsername(username);
+			dataSource.setPassword(password);
+			return dataSource;
+
+		} catch (Exception e) {
+			logger.error("DBCP datasource could not be created!", e);
+			return null;
+		}
+	}
+
+}
---
+++ Chapter6/src/test/java/com/utk/DataSourceConfigTest.java
@@ -0,0 +1,48 @@
+package com.utk;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+import javax.sql.DataSource;
+
+import org.junit.jupiter.api.Test;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.context.annotation.AnnotationConfigApplicationContext;
+
+import com.utk.config.SimpleDataSourceConfig;
+
+public class DataSourceConfigTest {
+
+	private static Logger logger = LoggerFactory.getLogger(DataSourceConfigTest.class);
+
+	@Test
+	public void testSimpleDataSource() throws SQLException {
+		AnnotationConfigApplicationContext context = new AnnotationConfigApplicationContext(
+				SimpleDataSourceConfig.class);
+		DataSource dataSource = context.getBean("dataSource", DataSource.class);
+		assertNotNull(dataSource);
+		testDataSource(dataSource);
+		context.close();
+
+	}
+
+	private void testDataSource(DataSource dataSource) throws SQLException {
+		try(Connection connection = dataSource.getConnection();
+			PreparedStatement statement = connection.prepareStatement("Select 1");
+				ResultSet resultSet = statement.executeQuery();
+		) {
+			while (resultSet.next()) {
+				int mockVal = resultSet.getInt("1");
+				assertEquals(1, mockVal);
+			}
+		} catch (Exception e) {
+			logger.debug("Something unusual happened.", e);
+		}
+	}
+}

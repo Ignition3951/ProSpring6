d925d8fbe45fc5c1245629334c05c13207163797
Utkarsh <ignition3951@gmail.com>
2025-02-10 12:29:31 +0530

    IMplement hibernate update

--- Chapter7/src/main/java/com/utk/HibernateDemoV1.java
+++ Chapter7/src/main/java/com/utk/HibernateDemoV1.java
@@ -1,7 +1,5 @@
 package com.utk;
 
-import java.time.LocalDate;
-
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.context.annotation.AnnotationConfigApplicationContext;
@@ -9,7 +7,6 @@ import org.springframework.context.annotation.AnnotationConfigApplicationContext
 import com.utk.config.HibernateConfig;
 import com.utk.dao.SingerDao;
 import com.utk.entity.Album;
-import com.utk.entity.Singer;
 
 public class HibernateDemoV1 {
 
@@ -24,19 +21,31 @@ public class HibernateDemoV1 {
 		logger.info("-----Listing singers with findAllWithAlbum");
 		singerDao.findAllWithAlbum().forEach(s -> logger.info(s.toString()));
 		logger.info("----Listing findById using named Query : {}", singerDao.findById(1l));
-		var singer = new Singer();
-		singer.setFistName("BB");
-		singer.setLastName("King");
-		singer.setBirthDate(LocalDate.of(1940, 8, 16));
-		var album = new Album();
-		album.setTitle("My Kind of Blues");
-		album.setReleaseDate(LocalDate.of(1961, 7, 18));
-		singer.addAlbum(album);
-		album = new Album();
-		album.setTitle("A Heart Full of Blues");
-		album.setReleaseDate(LocalDate.of(1962, 3, 20));
-		singer.addAlbum(album);
-		singerDao.save(singer);
+//		var singer = new Singer();
+//		singer.setFistName("BB");
+//		singer.setLastName("King");
+//		singer.setBirthDate(LocalDate.of(1940, 8, 16));
+//		var album = new Album();
+//		album.setTitle("My Kind of Blues");
+//		album.setReleaseDate(LocalDate.of(1961, 7, 18));
+//		singer.addAlbum(album);
+//		album = new Album();
+//		album.setTitle("A Heart Full of Blues");
+//		album.setReleaseDate(LocalDate.of(1962, 3, 20));
+//		singer.addAlbum(album);
+//		singerDao.save(singer);
+		var singerUpdate = singerDao.findById(4l);
+		Album updateAlbum = singerUpdate.getAlbums().stream().filter(a -> a.getTitle().equals("My Kind of Blues"))
+				.findFirst().orElse(null);
+		logger.info("----Album fetched : {}", updateAlbum.toString());
+		singerUpdate.setFistName("BBUPdate");
+		singerUpdate.setLastName("KingUPdate");
+		singerUpdate.removeALbum(updateAlbum);
+		// int beforeUpdateversion = singerUpdate.getVersion();
+		// logger.info("VERSION BEFORE THE UPDATE IS : {}", beforeUpdateversion);
+		singerDao.save(singerUpdate);
+		int afterUpdateversion = singerUpdate.getVersion();
+		logger.info("VERSION AFTER THE UPDATE IS : {}", afterUpdateversion);
 		context.close();
 
 	}

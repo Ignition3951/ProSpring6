1edce61bbd72aa6a7f8c94ed49dd8064ea2990dc
Utkarsh <ignition3951@gmail.com>
2025-03-18 16:11:43 +0530

    Implement jdbc implementation to fetch user data from database

--- ProChp14/src/main/java/com/utk/config/SecurityConfig.java
+++ ProChp14/src/main/java/com/utk/config/SecurityConfig.java
@@ -1,16 +1,16 @@
 package com.utk.config;
 
+import javax.sql.DataSource;
+
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 
 @Configuration
@@ -24,13 +24,22 @@ public class SecurityConfig {
 	}
 
 	@Bean
-	public UserDetailsService userDetailsService(PasswordEncoder passwordEncoder) {
-		User.UserBuilder users = User.builder().passwordEncoder(passwordEncoder::encode);
-		UserDetails john = users.username("john").password("password").roles("USER").build();
-		UserDetails jane = users.username("jane").password("password").roles("USER").build();
-		UserDetails admin = users.username("admin").password("admin").roles("ADMIN")
-				.build();
-		return new InMemoryUserDetailsManager(john, jane, admin);
+	public UserDetailsService userDetailsService(DataSource dataSource) {
+
+		/*
+		 * Uncomment this block for the first time to create users in the database
+		 * 
+		 * User.UserBuilder users =
+		 * User.builder().passwordEncoder(passwordEncoder()::encode); UserDetails john =
+		 * users.username("john").password("password").roles("USER").build();
+		 * UserDetails jane =
+		 * users.username("jane").password("password").roles("USER").build();
+		 * UserDetails admin = users.username("admin").password("admin").roles("ADMIN")
+		 * .build(); JdbcUserDetailsManager manager = new
+		 * JdbcUserDetailsManager(dataSource); manager.createUser(john);
+		 * manager.createUser(jane); manager.createUser(admin);
+		 */
+		return new JdbcUserDetailsManager(dataSource);
 	}
 
 	@Bean
